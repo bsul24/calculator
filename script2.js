2ccbcf91ee67d997350b19ae8bec7d463c6557ae
Bill Sullivan <w.o.sullivan24@gmail.com>
2022-10-18 22:09:58 -0700

    Create functionality for basic use cases

--- script2.js
+++ script2.js
@@ -18,8 +18,9 @@ const allBtns = [...document.querySelectorAll("button")];
 // State
 const state = {
   stored: "",
+  tempStored: "",
   operator: "",
-  lastButtonOperator: false,
+  lastKeyPressed: "",
 };
 
 // Functions
@@ -49,64 +50,102 @@ const operate = function (operator, a, b) {
 };
 
 const handleNumber = function (e) {
+  removeOperatorHighlights();
+
   const number = e.target.textContent;
-  if (display.textContent === "0") {
+  if (display.textContent === "0" || state.lastKeyPressed === "operator") {
     display.textContent = number;
   } else {
     display.textContent += number;
   }
+
+  state.lastKeyPressed = "number";
 };
 
 const handleDecimal = function () {
+  if (state.lastKeyPressed === "operator") {
+    display.textContent = "0.";
+    state.lastKeyPressed = "decimal";
+    return;
+  }
+
   const displayValue = display.textContent;
   for (let i = 0; i < displayValue.length; i++) {
     if (displayValue[i] === ".") return;
   }
-
+  state.lastKeyPressed = "decimal";
   display.textContent += ".";
 };
 
 const handleOperator = function (e) {
-  operatorBtns.forEach((btn) => btn.classList.remove("selected"));
+  removeOperatorHighlights();
+  if (state.stored && state.lastKeyPressed !== "operator") {
+    const result = operate(state.operator, state.stored, +display.textContent);
+    display.textContent = result;
+  }
+  state.lastKeyPressed = "operator";
   const button = e.target;
+
+  state.stored = +display.textContent;
   if (button === addBtn) {
     state.operator = add;
     addBtn.classList.add("selected");
   }
-  if (button === addBtn) {
-    state.operator = add;
-    addBtn.classList.add("selected");
+  if (button === subtractBtn) {
+    state.operator = subtract;
+    subtractBtn.classList.add("selected");
   }
-  if (button === addBtn) {
-    state.operator = add;
-    addBtn.classList.add("selected");
+  if (button === multiplyBtn) {
+    state.operator = multiply;
+    multiplyBtn.classList.add("selected");
   }
-  if (button === addBtn) {
-    state.operator = add;
-    addBtn.classList.add("selected");
+  if (button === divideBtn) {
+    state.operator = divide;
+    divideBtn.classList.add("selected");
   }
 };
 
 const handleEquals = function () {
-  if (!state.operator || !state.stored || !state.displayValue) return;
-  state.displayValue = operate(
-    state.operator,
-    state.stored,
-    +state.displayValue
-  );
-  display.textContent = state.displayValue;
+  if (state.stored === "") return;
+
+  if (state.lastKeyPressed === "equals") {
+    const result = operate(state.operator, state.stored, state.tempStored);
+    state.stored = result;
+    display.textContent = result;
+    return;
+  }
+
+  const result = operate(state.operator, state.stored, +display.textContent);
+  state.stored = result;
+  state.tempStored = +display.textContent;
+  display.textContent = result;
+  state.lastKeyPressed = "equals";
 };
 
 const handleClear = function () {
-  state.displayValue = "";
   state.stored = "";
+  state.tempStored = "";
   state.operator = "";
+  state.lastKeyPressed = "clear";
   display.textContent = "0";
+  removeOperatorHighlights();
+};
+
+const removeOperatorHighlights = function () {
+  operatorBtns.forEach((btn) => {
+    btn.classList.remove("selected");
+    btn.classList.remove("mousedown");
+  });
 };
 
 // Event listeners
 numberBtns.forEach((btn) => btn.addEventListener("click", handleNumber));
 operatorBtns.forEach((btn) => btn.addEventListener("click", handleOperator));
+operatorBtns.forEach((btn) =>
+  btn.addEventListener("mousedown", function () {
+    this.classList.add("mousedown");
+  })
+);
 equalBtn.addEventListener("click", handleEquals);
 clearBtn.addEventListener("click", handleClear);
 decimalBtn.addEventListener("click", handleDecimal);
