e017c4b4814c0aed6b217af110f417a660047df0
Bill Sullivan <w.o.sullivan24@gmail.com>
2022-10-19 17:39:28 -0700

    Reduce event listeners and functions, add functionality for every button

--- script2.js
+++ script2.js
@@ -2,18 +2,11 @@
 
 // DOM elements
 const display = document.querySelector(".display");
-const numberBtns = [...document.querySelectorAll(".num-btn")];
-const decimalBtn = document.querySelector(".decimal-btn");
 const operatorBtns = [...document.querySelectorAll(".operator-btn")];
 const addBtn = document.querySelector(".add-btn");
 const subtractBtn = document.querySelector(".subtract-btn");
 const multiplyBtn = document.querySelector(".multiply-btn");
 const divideBtn = document.querySelector(".divide-btn");
-const deleteBtn = document.querySelector(".delete-btn");
-const percentBtn = document.querySelector(".percent-btn");
-const signBtn = document.querySelector(".sign-btn");
-const equalBtn = document.querySelector(".equal-btn");
-const clearBtn = document.querySelector(".clear-btn");
 const allBtns = [...document.querySelectorAll("button")];
 
 // State
@@ -50,124 +43,6 @@ const operate = function (operator, a, b) {
   return operator(a, b);
 };
 
-const handleNumber = function (e) {
-  removeOperatorHighlights();
-
-  const number = e.target.textContent;
-  if (display.textContent === "0" || state.lastKeyPressed === "operator") {
-    display.textContent = number;
-  } else if (state.lastKeyPressed === "equals") {
-    display.textContent = number;
-    state.stored = "";
-  } else {
-    display.textContent += number;
-  }
-
-  state.lastKeyPressed = "number";
-};
-
-const handleDecimal = function () {
-  if (
-    state.lastKeyPressed === "operator" ||
-    state.lastKeyPressed === "equals"
-  ) {
-    if (state.lastKeyPressed === "equals") state.stored = "";
-    display.textContent = "0.";
-    state.lastKeyPressed = "decimal";
-    return;
-  }
-
-  const displayValue = display.textContent;
-  for (let i = 0; i < displayValue.length; i++) {
-    if (displayValue[i] === ".") return;
-  }
-  state.lastKeyPressed = "decimal";
-  display.textContent += ".";
-};
-
-const handleOperator = function (e) {
-  removeOperatorHighlights();
-  if (
-    state.stored &&
-    state.lastKeyPressed !== "operator" &&
-    state.lastKeyPressed !== "equals"
-  ) {
-    const result = operate(state.operator, state.stored, +display.textContent);
-    display.textContent = result;
-  }
-  if (state.lastKeyPressed !== "equals") {
-    state.stored = +display.textContent;
-  }
-
-  state.lastKeyPressed = "operator";
-  const button = e.target;
-
-  if (button === addBtn) {
-    state.operator = add;
-    addBtn.classList.add("selected");
-  }
-  if (button === subtractBtn) {
-    state.operator = subtract;
-    subtractBtn.classList.add("selected");
-  }
-  if (button === multiplyBtn) {
-    state.operator = multiply;
-    multiplyBtn.classList.add("selected");
-  }
-  if (button === divideBtn) {
-    state.operator = divide;
-    divideBtn.classList.add("selected");
-  }
-};
-
-const handleDelete = function () {
-  if (display.textContent === "0" || display.textContent === "-0") return;
-
-  if (state.lastKeyPressed === "operator") {
-    display.textContent = "0";
-  } else if (state.lastKeyPressed === "equals") {
-    display.textContent = "0";
-    state.stored = "";
-  } else if (
-    +display.textContent >= -9 &&
-    +display.textContent <= 9 &&
-    Number.isInteger(+display.textContent) &&
-    display.textContent.slice(-1) !== "."
-  ) {
-    display.textContent = "0";
-  } else {
-    display.textContent = display.textContent.slice(0, -1);
-  }
-
-  state.lastKeyPressed = "delete";
-};
-
-const handleEquals = function () {
-  if (state.stored === "") return;
-
-  if (state.lastKeyPressed === "equals") {
-    const result = operate(state.operator, state.stored, state.tempStored);
-    state.stored = result;
-    display.textContent = result;
-    return;
-  }
-
-  const result = operate(state.operator, state.stored, +display.textContent);
-  state.stored = result;
-  state.tempStored = +display.textContent;
-  display.textContent = result;
-  state.lastKeyPressed = "equals";
-};
-
-const handleClear = function () {
-  state.stored = "";
-  state.tempStored = "";
-  state.operator = "";
-  state.lastKeyPressed = "clear";
-  display.textContent = "0";
-  removeOperatorHighlights();
-};
-
 const removeOperatorHighlights = function () {
   operatorBtns.forEach((btn) => {
     btn.classList.remove("selected");
@@ -175,15 +50,225 @@ const removeOperatorHighlights = function () {
   });
 };
 
+const getButtonType = function (e) {
+  if (e.target.classList.contains("num-btn")) return "number";
+  if (e.target.classList.contains("decimal-btn")) return "decimal";
+  if (e.target.classList.contains("operator-btn")) return "operator";
+  if (e.target.classList.contains("delete-btn")) return "delete";
+  if (e.target.classList.contains("sign-btn")) return "sign";
+  if (e.target.classList.contains("percent-btn")) return "percent";
+  if (e.target.classList.contains("equal-btn")) return "equals";
+  if (e.target.classList.contains("clear-btn")) return "clear";
+};
+
+const createDisplayValue = function (e, buttonType) {
+  if (buttonType === "number") {
+    const number = e.target.textContent;
+    if (display.textContent === "0" || state.lastKeyPressed === "operator") {
+      return number;
+    } else if (display.textContent === "-0") {
+      return "-" + number;
+    } else if (state.lastKeyPressed === "equals") {
+      return number;
+    } else {
+      return display.textContent + number;
+    }
+  }
+
+  if (buttonType === "decimal") {
+    if (
+      state.lastKeyPressed === "operator" ||
+      state.lastKeyPressed === "equals"
+    ) {
+      return "0.";
+    }
+
+    const displayValue = display.textContent;
+    for (let i = 0; i < displayValue.length; i++) {
+      if (displayValue[i] === ".") return displayValue;
+    }
+    return display.textContent + ".";
+  }
+
+  if (buttonType === "operator") {
+    if (
+      state.stored &&
+      state.lastKeyPressed !== "operator" &&
+      state.lastKeyPressed !== "equals"
+    ) {
+      return operate(state.operator, state.stored, +display.textContent);
+    }
+
+    return display.textContent;
+  }
+
+  if (buttonType === "equals") {
+    if (state.stored === "") return display.textContent;
+
+    if (state.lastKeyPressed === "equals") {
+      return operate(state.operator, state.stored, state.tempStored);
+    }
+
+    updateState(e);
+    return operate(state.operator, state.stored, +display.textContent);
+  }
+
+  if (buttonType === "clear") {
+    return (display.textContent = "0");
+  }
+
+  if (buttonType === "delete") {
+    if (display.textContent === "0" || display.textContent === "-0")
+      return display.textContent;
+
+    if (
+      state.lastKeyPressed === "operator" ||
+      state.lastKeyPressed === "equals"
+    ) {
+      return "0";
+    } else if (
+      +display.textContent >= -9 &&
+      +display.textContent <= 9 &&
+      Number.isInteger(+display.textContent) &&
+      display.textContent.slice(-1) !== "."
+    ) {
+      return "0";
+    } else {
+      return display.textContent.slice(0, -1);
+    }
+  }
+
+  if (buttonType === "percent") {
+    return +display.textContent / 100;
+  }
+
+  if (buttonType === "sign") {
+    if (display.textContent[0] === "-") {
+      return display.textContent.slice(1);
+    } else {
+      return "-" + display.textContent;
+    }
+  }
+};
+
+const updateUI = function (e) {
+  const buttonType = getButtonType(e);
+  display.textContent = createDisplayValue(e, buttonType);
+  if (buttonType === "number") {
+    removeOperatorHighlights();
+  }
+
+  if (buttonType === "operator") {
+    removeOperatorHighlights();
+
+    if (e.target === addBtn) {
+      addBtn.classList.add("selected");
+    }
+
+    if (e.target === subtractBtn) {
+      subtractBtn.classList.add("selected");
+    }
+
+    if (e.target === multiplyBtn) {
+      multiplyBtn.classList.add("selected");
+    }
+
+    if (e.target === divideBtn) {
+      divideBtn.classList.add("selected");
+    }
+  }
+
+  if (buttonType === "clear") {
+    removeOperatorHighlights();
+  }
+};
+
+const updateState = function (e) {
+  const buttonType = getButtonType(e);
+  if (buttonType === "number") {
+    if (state.lastKeyPressed === "equals") {
+      state.stored = "";
+      state.operator = "";
+      state.tempStored = "";
+    }
+  }
+
+  if (buttonType === "decimal") {
+    if (state.lastKeyPressed === "equals") {
+      state.stored = "";
+    }
+  }
+
+  if (buttonType === "operator") {
+    if (state.lastKeyPressed !== "equals") {
+      state.stored = +display.textContent;
+    }
+
+    if (e.target === addBtn) {
+      state.operator = add;
+    }
+
+    if (e.target === subtractBtn) {
+      state.operator = subtract;
+    }
+    if (e.target === multiplyBtn) {
+      state.operator = multiply;
+    }
+    if (e.target === divideBtn) {
+      state.operator = divide;
+    }
+  }
+
+  if (buttonType === "equals") {
+    if (state.stored === "") return;
+
+    if (state.lastKeyPressed !== "equals") {
+      state.tempStored = +display.textContent;
+      state.lastKeyPressed = buttonType;
+      return;
+    }
+
+    state.stored = +display.textContent;
+  }
+
+  if (buttonType === "clear") {
+    state.stored = "";
+    state.tempStored = "";
+    state.operator = "";
+  }
+
+  if (buttonType === "delete") {
+    if (state.lastKeyPressed === "equals") {
+      state.stored = "";
+    }
+  }
+
+  if (buttonType === "percent") {
+    if (state.lastKeyPressed === "equals") {
+      state.stored = +display.textContent;
+      return;
+    }
+  }
+
+  if (buttonType === "sign") {
+    if (state.lastKeyPressed === "equals") {
+      state.stored = +display.textContent;
+      return;
+    }
+  }
+
+  state.lastKeyPressed = buttonType;
+};
+
 // Event listeners
-numberBtns.forEach((btn) => btn.addEventListener("click", handleNumber));
-operatorBtns.forEach((btn) => btn.addEventListener("click", handleOperator));
+allBtns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    updateUI(e);
+    updateState(e);
+  })
+);
 operatorBtns.forEach((btn) =>
   btn.addEventListener("mousedown", function () {
     this.classList.add("mousedown");
   })
 );
-deleteBtn.addEventListener("click", handleDelete);
-equalBtn.addEventListener("click", handleEquals);
-clearBtn.addEventListener("click", handleClear);
-decimalBtn.addEventListener("click", handleDecimal);
